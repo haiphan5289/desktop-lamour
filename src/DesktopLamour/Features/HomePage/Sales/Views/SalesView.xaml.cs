517b292abf1de57a11f73b6959bf331114f6af3b
Hai Phan <hai.phan@chotot.vn>
2026-08-26 21:01:21 +0700

    [CS] revert báo cáo in "Bán hàng" screen

--- src/DesktopLamour/Features/HomePage/Sales/Views/SalesView.xaml.cs
+++ src/DesktopLamour/Features/HomePage/Sales/Views/SalesView.xaml.cs
@@ -1,24 +1,13 @@
 // Copyright © 2026 DesktopLamour. All rights reserved.
-using System.Windows.Input;
-using DesktopLamour.Features.HomePage.Sales.Domain.Models;
 using DesktopLamour.Features.HomePage.Sales.ViewModels;
 
 namespace DesktopLamour.Features.HomePage.Sales.Views;
 
 public partial class SalesView : System.Windows.Controls.UserControl
 {
-    private SalesViewModel ViewModel => (SalesViewModel)DataContext;
-
     public SalesView(SalesViewModel viewModel)
     {
         InitializeComponent();
         DataContext = viewModel;
-        Loaded += async (_, _) => await viewModel.InitializeReportPanelCommand.ExecuteAsync(null);
-    }
-
-    private void ReportRowsGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-    {
-        if (ReportRowsGrid.SelectedItem is ProductSalesSummaryRow row && ViewModel.DrillDownReportRowCommand.CanExecute(row))
-            ViewModel.DrillDownReportRowCommand.Execute(row);
     }
 }
